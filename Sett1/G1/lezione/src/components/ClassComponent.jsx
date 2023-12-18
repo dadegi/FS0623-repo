7e99333ae27d97fa34af975aa73e1d72ac372c9a
Naviganet <dadegi@gmail.com>
2023-12-18 13:08:36 +0100

    Codice lezione UNIT 3 settimana 1 giorno 1

---
+++ Sett1/G1/lezione/src/components/ClassComponent.jsx
@@ -0,0 +1,11 @@
+// Componente a classe
+
+import { Component } from "react";
+
+class ClassComponent extends Component {
+    render() {
+        return <h3>Class Component</h3>
+    }
+}
+
+export default ClassComponent;
---
+++ Sett1/G1/lezione/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<div style={{ display: 'flex', justifyContent: 'space-around' }}>
+			<div>EPICODE - Corso FS0623A</div>
+		</div>
+	);
+};
+
+export default Footer;
