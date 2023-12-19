929acddcbc9832e15fcb65387764671a6276b14e
Naviganet <dadegi@gmail.com>
2023-12-19 11:01:32 +0100

    Codice lezione UNIT 3 settimana 1 giorno 2

--- Sett1/G2/lezione/src/App.js
+++ Sett1/G2/lezione/src/App.js
@@ -7,9 +7,12 @@ import Testo from './components/Testo';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+		// Il componente app istanzia testoRicerca come state e fa il bind del metodo modificaTesto
 		this.state = { testoRicerca: '' };
 		this.modificaTesto = this.modificaTesto.bind(this);
 	}
+
+	// 5. Il metodo modificaTesto, che riceve come parametro dal component Testo il value scritto nel campo di input (riga 22 component Testo), usa questo parametro per fare il setState di testoRicerca
 
 	modificaTesto(testo) {
 		this.setState({testoRicerca: testo});
@@ -26,6 +29,7 @@ class App extends React.Component {
 					<hr />
 					<StateClass />
 					<hr />
+					{/* 4. Al component Testo viene passato come props il metodo onChangeTesto, che a sua volta richiama il metodo modificaTesto, mentre la props testoRicerca riceve come valore il valore dello state testoRicerca */}
 					<Testo onChangeTesto = {this.modificaTesto} testoRicerca = {this.state.testoRicerca} />
 					<h3>{this.state.testoRicerca}</h3>
 				</main>
--- Sett1/G2/lezione/src/components/Testo.jsx
+++ Sett1/G2/lezione/src/components/Testo.jsx
@@ -3,18 +3,22 @@ import React from "react";
 class Testo extends React.Component {
     constructor(props) {
         super(props);
+        // Il bind serve per popolare con valori e flusso di istruzioni l'oggetto this
         this.cambiaTesto = this.cambiaTesto.bind(this);
     }
 
+    // 3. Il metodo cambiaTesto chiama il metodo onChangeTesto ricevuto come props dal componente App e li passa come parametro il nuovo value del campo di input
     cambiaTesto(e) {
         this.props.onChangeTesto(e.target.value);
     }
 
     render() {
+        // 1. Il component riceve come props testoRicerca e lo usa come value del campo di input
         const testoRicerca = this.props.testoRicerca;
         return (
             <form>
                 <label for="testoRicerca">Testo ricerca</label>
+                {/* 2. Quando nel campo di input si cambia valore, viene chiamato il metodo cambiaTesto */}
                 <input type="text" name="testoRicerca" id="testoRicerca" value={testoRicerca} onChange={this.cambiaTesto} />
             </form>
         )
