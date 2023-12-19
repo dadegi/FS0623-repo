e763842b1a0cd86afaf326c8ed97f98cc985a621
Naviganet <dadegi@gmail.com>
2023-12-19 12:51:55 +0100

    Codice UNIT 3 settimana 1 - Ristorante

--- Sett1/ristorante/src/App.js
+++ Sett1/ristorante/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './components/CustomNavbar';
+import Home from './components/Home';
+
 
 function App() {
 	return (
 		<div>
 			<CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
+			<Home />
 		</div>
 	);
 }
