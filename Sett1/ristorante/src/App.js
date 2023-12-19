a53c6e680cb49ae26d44f91ebd8abb4470390b77
Naviganet <dadegi@gmail.com>
2023-12-19 12:11:00 +0100

    Codice UNIT 3 settimana 1 - Ristorante

--- Sett1/ristorante/src/App.js
+++ Sett1/ristorante/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavbar from './components/CustomNavbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
+		</div>
+	);
 }
 
 export default App;
